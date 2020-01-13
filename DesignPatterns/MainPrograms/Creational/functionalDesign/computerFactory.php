cf15bc004e98dedb346037a986e7a59e6987d2d6
[skfirozz] <[shaikfiroz838@gmail.com]>
2020-01-13 18:59:49 +0530

    [skfirozz]added.factoryDesign program

--- DesignPatterns/MainPrograms/Creational/functionalDesign/computerFactory.php
+++ DesignPatterns/MainPrograms/Creational/functionalDesign/computerFactory.php
@@ -1,4 +1,41 @@
 <?php
-class ComputerFactory{
-    
+include '/home/admin1/Documents/Fellowship/Design Patterns/DesignPatterns/Utility/utility.php';
+include '/home/admin1/Documents/Fellowship/Design Patterns/DesignPatterns/MainPrograms/Creational/functionalDesign/pc.php';
+include '/home/admin1/Documents/Fellowship/Design Patterns/DesignPatterns/MainPrograms/Creational/functionalDesign/laptop.php';
+include '/home/admin1/Documents/Fellowship/Design Patterns/DesignPatterns/MainPrograms/Creational/functionalDesign/server.php';
+class ComputerFactory
+{
+    public function getInfo($n)
+    {
+        switch ($n) {
+            case 1:echo "YOUR LAPTOP DETAILS ARE : \n";
+                return new Laptop('Dell', '8GB', '1TB');
+                break;
+            case 2:echo "YOUR PC DETAILS ARE : ";
+                return new PC('Hp', '4GB', '500GB', 'Windows 10');
+                break;
+            case 3:echo "YOUR SERVER DETAILS ARE : ";
+                return new Server('JamesBond', 'H.S.R Layout', '172.23.200.10');
+                break;
+            default:
+                echo ".........EXIT....¶¶¶¶¶¶¶\n";
+                break;
+        }
+    }
+}
+function run()
+{
+    $obj=new ComputerFactory();
+    echo "enter  \n1.Laptop\t2.PC\t3.Server : ";
+    $reference=$obj->getInfo(Utility::getInt());
+    $ref = new ReflectionClass($obj);
+    $dfdf = $ref->getProperties() ;
+    foreach($ref->getProperties() as $key){
+        echo $key->getName()."->".$key->getValue($obj)."\n";
+    } 
+}
+run();
+?>
+  
+
 }
--- DesignPatterns/Utility/utility.php
+++ DesignPatterns/Utility/utility.php
@@ -1,4 +1,13 @@
 <?php
-class Utility{
+class Utility
+{
+    public static function getInt()
+    {
+        fscanf(STDIN, "%d\n", $n);
+        while (!is_numeric($n)) {
+            echo "enter numeric value" . "\n";
+            fscanf(STDIN, "%d\n", $n);
+        }
+        return $n;
+    }
 }
-?>
